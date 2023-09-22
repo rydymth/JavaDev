1d499b38e6380c97f78c9b3cbf0d7a444f1bfd94
rydymth <rudra.pednekar@gmail.com>
2023-09-23 01:29:41 +0530

    DFS successfully Implemented and concepts understood. Finish off question

--- problemSolving/Swayam/Assignments/AssignmentsWeek3/dfs.java
+++ problemSolving/Swayam/Assignments/AssignmentsWeek3/dfs.java
@@ -91,20 +91,22 @@ class linkedList {
 class graph {
   boolean flagDone = false;
   linkedList l [];
-  linkedList dfsNodes [];
+  linkedList [] res;
+  int count = 0;
   boolean [] visited;
+  boolean doneFlag = false;
   int nV;
 
   graph (int v) {
     this.nV = v;
     l = new linkedList[v];
-    dfsNodes = new linkedList[v];
-    visited = new boolean[nV];
+    res = new linkedList[v];
+    visited = new boolean[v];
     for (int i = 0; i < v; i++)
     {
       visited[i] = false;
       l[i] = new linkedList();
-      dfsNodes[i] = new linkedList();
+      res[i] = new linkedList();
     }
   }
 
@@ -124,63 +126,82 @@ class graph {
     }
     return ret;
   }
-
-  void DFSstack () {
-    this.flagDone = true;
-    for (int s = 0; s < this.nV; s++) {
-      linkedList dfsNodes = new linkedList();
-      stack st = new stack(nV);
-      st.push(s);
-      visited[s] = true;
-      while (st.getSize() != 0) {
-        int t = st.pop();
-        dfsNodes.addNode(t);
-        int [] tmp = adjList(t);
-      for (int i = 0; i < tmp.length; i++)
-        {
-          l[tmp[i]].nodeID = s;
-          if (visited[tmp[i]] == false) {      
-            visited[tmp[i]] = true;
-            st.push(tmp[i]);
+  
+  linkedList DFSstack (int s) { 
+    linkedList dfsNodes = new linkedList();
+    stack st = new stack(nV);
+    st.push(s);
+    visited[s] = true;
+    while (st.getSize() != 0) {
+      int t = st.pop();
+      dfsNodes.addNode(t);
+      int [] tmp = adjList(t);
+    for (int i = 0; i < tmp.length; i++)
+      {
+        l[tmp[i]].nodeID = s;
+        if (visited[tmp[i]] == false) {
+          visited[tmp[i]] = true;
+          st.push(tmp[i]);
         }
       }
     }
+    return dfsNodes;
+  }
+  
+  // We should never implicitly call the dfsStack function. Only use this global function to compute dfs globally.
+  void dfsNodes () {
+    for (int i = 0; i < this.nV; i++) {
+      if (!visited[i]) {
+        this.count++;
+        res[i] = this.DFSstack(i);
+      }
     }
+  }
+ 
+  int numConnected () {
+    if (!flagDone)
+      this.dfsNodes();
+    return this.count;
   }
   
   void getDfsNodes (int s) {
-    if (this.flagDone == false)
-      this.DFSstack();
-    else {
-      node tmp = this.dfsNodes[s].head;
-      while (tmp != null) {
-        if (tmp.next == null) {
-          System.out.print((tmp.Data + 1));
-          return;
-        }
-        else { System.out.print((tmp.Data + 1)+ " -> "); }
-        tmp = tmp.next;
-    }
+    if (!flagDone)
+      this.dfsNodes();
+    linkedList dfsNodes = this.res[s];
+    node tmp = dfsNodes.head;
+    while (tmp != null) {
+      if (tmp.next == null) {
+        System.out.print((tmp.Data + 1));
+        return;
+      }
+      else { System.out.print((tmp.Data + 1)+ " -> "); }
+      tmp = tmp.next;
     }
   }
 }
 
 public class dfs {
+  
+  public static min (int [] arr) {
+    
+  }
+
   public static void main(String [] args)
   {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
     int M = sc.nextInt();
     int K = sc.nextInt();
-    graph g = new graph(N + 2);
+    graph g = new graph(N);
     for (int i = 0; i < M; i++) {
       g.addEdge(sc.nextInt(), sc.nextInt());
     }
-    
-    g.DFSstack();
-    for (int i = 0; i < g.nV; i++)
+    System.out.println(g.numConnected());
+    System.out.println(g.res.length);
+    for (int i = 0; i < g.res.length; i++)
     {
-      System.out.print((g.l[i].nodeID + 1) + "->");
+      g.getDfsNodes(i);
+      System.out.println();
     }
   }
 }
